a087f81b4b6eac03d33827f83379b100a346cc20
Santiago <santiagodmaldon@gmail.com>
2022-03-09 21:33:44 -0400

    Get Posts With Api

--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -1,45 +1,35 @@
 <template>  
-    <div class="posts">
-      <div class=" post-box bg-white" >
-        <div class="post-header p-2 ">
-          <div class="avatar-post">
-            <a href="">
-              <img src="../assets/img/profile1.png" alt="Avatar Profile"> Username
-              </a>
+    <div class="" v-if="posts" >
+      <div class="posts" v-for="post in posts" :key="post.id">
+        <div class=" post-box bg-white"    >
+          <div class="post-header p-2 ">
+            <div class="avatar-post">
+              <router-link to="/profile/id">
+                <img src="../assets/img/profile1.png" alt="Avatar Profile"> {{post.user.username}}
+                </router-link>
+            </div>
+            <div class="created_at "> {{post.created_at}}
+            </div>
           </div>
-          <div class="created_at ">
-            11/01/2022
+          <div class="image-box">
+          <img  src="../assets/img/2.jpg" class="card-img-top" alt="...">
           </div>
-        </div>
-        <div class="image-box">
-        <img  src="../assets/img/2.jpg" class="card-img-top" alt="...">
-        </div>
-        <div class="card-body">
-          <h5 class="card-title fw-bold">Card title</h5>
-          <p class="card-text"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate at quisquam dolore rem necessitatibus quae rerum commodi, corrupti voluptatem perspiciatis quidem sed laboriosam quas possimus, quos explicabo! Quia, nobis ducimus. Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <div class="buttons-post d-flex">
-            <div class=""> <a href="" class="text-success fs-5"><i class="fas fa-thumbs-up"></i> 250</a></div>
-            <div class=" mx-3"> <a href="" class="text-danger fs-5"><i class="fas fa-thumbs-down"></i> 255</a></div>
+          <div class="card-body">
+            <h5 class="card-title fw-bold">{{post.title}}</h5>
+            <p class="card-text">  {{post.content}} </p>
+            <div class="buttons-post d-flex">
+              <div class=""> <a href="" class="text-success fs-5"><i class="fas fa-thumbs-up"></i> 250</a></div>
+              <div class=" mx-3"> <a href="" class="text-danger fs-5"><i class="fas fa-thumbs-down"></i> 255</a></div>
 
-        
+          
 
+            </div>
           </div>
         </div>
-      </div>
-       <div class=" post-box" >
-        <div class="card-header">
-          <a href="#">Username</a>
-        </div>
-        <div class="image-box">
-        <img  src="../assets/img/1.jpg" class="card-img-top" alt="...">
-        </div>
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div> 
+      
+      </div>  
     </div>
+    
 
 </template>
 
@@ -49,7 +39,28 @@
 
 
 <script>
+import axios from 'axios';
 export default {
-    name: 'Posts'
+    name: 'Posts',
+    mounted(){
+      this.getPosts();
+    },
+    data(){
+      return{
+        posts : [],
+      }
+    },
+    methods: {
+      getPosts(){
+        axios.get('http://127.0.0.1:8000/api/post')
+              .then(res => {
+               
+               if(res.data.status == 'success'){
+                 this.posts = res.data.posts;
+                 console.log(this.posts);
+               }
+              });
+      }
+    }
 }
 </script>
